c66fc3fac06da6e64e87d5343d79161c2fed840c
hweey <hweey@HYLABTOP>
2023-12-06 07:44:01 +0800

    Add project files.

---
+++ FoodNutritionWebsite/Server/Models/ApplicationUser.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace FoodNutritionWebsite.Server.Models
+{
+    public class ApplicationUser : IdentityUser
+    {
+        public String? FirstName { get; set; }
+        public String? SecondName { get; set; }
+
+    }
+}
