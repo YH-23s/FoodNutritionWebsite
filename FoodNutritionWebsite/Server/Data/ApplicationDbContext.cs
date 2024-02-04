f2f5e30cfaff218a4c15cff2c531be806b5de47a
TONG HWEE YANG 2200776I <2200776I@student.tp.edu.sg>
2024-02-04 20:53:20 +0800

    changes made

--- FoodNutritionWebsite/Server/Data/ApplicationDbContext.cs
+++ FoodNutritionWebsite/Server/Data/ApplicationDbContext.cs
@@ -18,7 +18,7 @@ namespace FoodNutritionWebsite.Server.Data
 		public DbSet<AddNutrition> AddNutritions { get; set; }
         public DbSet<Food> Foods { get; set; }
         public DbSet<User> Usersssss { get; set; }
-        public DbSet<FoodAddedL> FoodAddedLs { get; set; }
+        
         public DbSet<FoodAddedMP> FoodAddedMPs { get; set; }
         public DbSet<FoodLog> FoodLogs { get; set; }
         public DbSet<FoodNutrition> FoodNutritions { get; set; }
