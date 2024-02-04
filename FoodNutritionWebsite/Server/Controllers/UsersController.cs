1c998d6c947183901941b4cf2faab87fc7171e58
TPJoshua <148735178+TPJoshua@users.noreply.github.com>
2024-02-04 20:43:49 +0800

    Fixed formcomponent for subscription

--- FoodNutritionWebsite/Server/Controllers/UsersController.cs
+++ FoodNutritionWebsite/Server/Controllers/UsersController.cs
@@ -30,7 +30,7 @@ namespace FoodNutritionWebsite.Server.Controllers
         }
 
 		// GET: api/Users
-		[Authorize(Roles = "User")]
+		[Authorize]
 		[HttpGet]
         public async Task<IActionResult> GetUsersssss()
         {
