52738cc727d13ce6178425e5e993c5671d5d12ca
TPJoshua <148735178+TPJoshua@users.noreply.github.com>
2024-02-07 10:20:08 +0800

    Last Update

--- FoodNutritionWebsite/Shared/Domain/FoodLog.cs
+++ FoodNutritionWebsite/Shared/Domain/FoodLog.cs
@@ -14,7 +14,6 @@ namespace FoodNutritionWebsite.Shared.Domain
         public double Quantity { get; set; }
         public DateTime? FoodLogDateTime { get; set; }
         public string? FoodType { get; set; }
-
         public string? FoodName { get; set; }
         public int? UserID { get; set; }
         public virtual User? User { get; set; }
